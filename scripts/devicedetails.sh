998f8e03e25e989ddde8f9cbec08bcdc764488c5
Zachary Flight <zachary@grotonbiblechapel.org>
2024-02-05 12:33:58 -0500

    Testing Updates

--- scripts/devicedetails.sh
+++ scripts/devicedetails.sh
@@ -4,5 +4,7 @@ sleep 30
 
 . /var/www/html/pss/conf/pss.conf
 
+log=$(date -I)
 mac=$(cat /sys/class/net/wlan0/address | sed 's/://g')
-curl -Ss "http://$database_ip/pss/scripts/dbupdate.php?type=devicedetails&device=$mac&devname=$HOSTNAME"
+
+curl -Ss "http://$database_ip/pss/scripts/dbupdate.php?type=devicedetails&device=$mac&devname=$HOSTNAME" >> /home/pi/log/$log.log
--- scripts/ipcheck.sh
+++ scripts/ipcheck.sh
@@ -18,7 +18,7 @@ waniplength=${#wanip}
 
 if [ $waniplength > 5 ] && [ $laniplength > 5 ]
 then
-  if [[ $curwan != $wanip ]] || [[ $curlan != $lanip ]]
+  if [ $curwan != $wanip ] || [ $curlan != $lanip ]
   then
     if [[ $pushover_configured == "yes" ]]
     then

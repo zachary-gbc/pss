c3bd6b265b936c7394eb1d84b316a16587858196
Zachary Flight <zachary@grotonbiblechapel.org>
2024-02-12 19:21:55 -0500

    Testing Updates

--- website/pss/other/footer.php
+++ website/pss/other/footer.php
@@ -1,7 +1,7 @@
 <br />
 <div class="footer">
 	<?php echo($systemname); ?>
-	<?php if((time() - $loadnow) == 1) { $s=""; } else { $s="s"; } echo("<br>Page Load Time: " . (time() - $loadnow) . " Second$s\n"); ?>
+	<?php #if((time() - $loadnow) == 1) { $s=""; } else { $s="s"; } echo("<br>Page Load Time: " . (time() - $loadnow) . " Second$s\n"); ?>
 </div>
 </body>
 

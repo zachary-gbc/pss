5b6218c41cad2a99e63955f7b0ff139108f62fd4
Zachary Flight <zachary@grotonbiblechapel.org>
2025-06-04 18:38:37 -0400

    Updates for converting videos

--- website/pss/scripts/createloop.php
+++ website/pss/scripts/createloop.php
@@ -10,15 +10,21 @@
 	{
 		foreach($aloopstobuild as $loopid => $orientation)
 		{
-			$graphics="SELECT Gr_ID, Gr_Name FROM AutomaticLoopDates INNER JOIN Graphics ON AutomaticLoopDates.AD_Graphic=Graphics.Gr_ID WHERE (AD_Loop='$loopid') AND ((AD_Date='$todaydate') OR (AD_Month='$month') OR ((AD_StartDateRange<='$todaydate') AND (AD_EndDateRange>='$todaydate')))"; $filecontents="";
+			$graphics="SELECT Gr_ID, Gr_Name FROM AutomaticLoopDates INNER JOIN Graphics ON AutomaticLoopDates.AD_Graphic=Graphics.Gr_ID WHERE (AD_Loop='$loopid') AND ((AD_Date='$todaydate') OR (AD_Month='$month') OR ((AD_StartDateRange<='$todaydate') AND (AD_EndDateRange>='$todaydate')))"; $vlcfilecontents=""; $concatfilecontents="";
 			if(!$rs=mysqli_query($db,$graphics)) { echo("Unable to Run Query: $graphics"); exit; }
 			while($row = mysqli_fetch_array($rs))
 			{
 				$graphicid=$row['Gr_ID']; $exists=false;
-				if(file_exists("/var/www/html/pss/files/$graphicid-$orientation.mp4")) { $filecontents.="/var/www/html/pss/files/$graphicid" . "-$orientation.mp4\n"; $exists=true; }
+				if(file_exists("/var/www/html/pss/files/$graphicid-$orientation.mp4"))
+				{
+					$vlcfilecontents.="/var/www/html/pss/files/$graphicid" . "-$orientation.mp4\n";
+					$concatfilecontents.="file '/var/www/html/pss/files/$graphicid" . "-$orientation.mp4'\n";
+					$exists=true;
+				}
 				if($exists == false) { $amissing[$loopid][$graphicid]=$row['Gr_Name']; }
 			}
-			if($filecontents != "") { file_put_contents("/var/www/html/pss/files/loop-$loopid.m3u", $filecontents); }
+			if($vlcfilecontents != "") { file_put_contents("/var/www/html/pss/files/loop-$loopid.m3u", $vlcfilecontents); }
+			if($concatfilecontents != "") { file_put_contents("/var/www/html/pss/files/loop-$loopid.concat", $concatfilecontents); }
 		}
 	}
 
@@ -30,15 +36,21 @@
 	{
 		foreach($mloopstobuild as $loopid => $orientation)
 		{
-			$graphics="SELECT Gr_ID, Gr_Name FROM LoopGraphics INNER JOIN Graphics ON LoopGraphics.LG_Graphic=Graphics.Gr_ID WHERE (LG_Loop='$loopid') ORDER BY LG_Order"; $filecontents="";
+			$graphics="SELECT Gr_ID, Gr_Name FROM LoopGraphics INNER JOIN Graphics ON LoopGraphics.LG_Graphic=Graphics.Gr_ID WHERE (LG_Loop='$loopid') ORDER BY LG_Order"; $vlcfilecontents=""; $concatfilecontents="";
 			if(!$rs=mysqli_query($db,$graphics)) { echo("Unable to Run Query: $graphics"); exit; }
 			while($row = mysqli_fetch_array($rs))
 			{
 				$graphicid=$row['Gr_ID']; $exists=false;
-				if(file_exists("/var/www/html/pss/files/$graphicid-$orientation.mp4")) { $filecontents.="/var/www/html/pss/files/$graphicid" . "-$orientation.mp4\n"; $exists=true; }
+				if(file_exists("/var/www/html/pss/files/$graphicid-$orientation.mp4"))
+				{
+					$vlcfilecontents.="/var/www/html/pss/files/$graphicid" . "-$orientation.mp4\n";
+					$concatfilecontents.="file '/var/www/html/pss/files/$graphicid" . "-$orientation.mp4'\n";
+					$exists=true;
+				}
 				if($exists == false) { $mmissing[$loopid][$graphicid]=$row['Gr_Name']; }
 			}
-			if($filecontents != "") { file_put_contents("/var/www/html/pss/files/loop-$loopid.m3u", $filecontents); }
+			if($vlcfilecontents != "") { file_put_contents("/var/www/html/pss/files/loop-$loopid.m3u", $vlcfilecontents); }
+			if($concatfilecontents != "") { file_put_contents("/var/www/html/pss/files/loop-$loopid.concat", $concatfilecontents); }
 		}
 	}
 

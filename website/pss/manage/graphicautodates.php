a8dc1ff260828a7532cec7fe5d6e45690ad81d41
Zachary Flight <zachary@grotonbiblechapel.org>
2025-06-07 12:47:49 -0400

    Fixed issue in graphicautodates

--- website/pss/manage/graphicautodates.php
+++ website/pss/manage/graphicautodates.php
@@ -1,8 +1,9 @@
 <?php
   include('../scripts/dblogin.php'); $form="";
-  $allmonths="<option value='0'><option><option value='1'>January</option><option value='2'>February</option><option value='3'>March</option><option value='4'>April</option>";
-  $allmonths.="<option value='5'>May</option><option value='6'>June</option><option value='7'>July</option><option value='8'>August</option><option value='9'>September</option>";
-  $allmonths.="<option value='10'>October</option><option value='11'>November</option><option value='12'>December</option>";
+  $allmonths="<option value='0'></option><option value='1'>January</option><option value='2'>February</option><option value='3'>March</option>";
+  $allmonths.="<option value='4'>April</option><option value='5'>May</option><option value='6'>June</option><option value='7'>July</option>";
+  $allmonths.="<option value='8'>August</option><option value='9'>September</option><option value='10'>October</option>";
+  $allmonths.="<option value='11'>November</option><option value='12'>December</option>";
 
   if(isset($_GET['graphic']) && isset($_GET['loop']))
   {

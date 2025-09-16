57e0bae9ebcf0f68cff0016b2f910c291fc4aea3
Zachary Flight <zachary@grotonbiblechapel.org>
2025-09-16 19:52:06 -0400

    Added conversion for non 16:9 images

--- scripts/videofromimage.sh
+++ scripts/videofromimage.sh
@@ -12,6 +12,7 @@ then
   query=$(mysql --user="$database_username" --password="$database_password" --database="$database_name" -N -e "UPDATE Variables SET Var_Value='1' WHERE (Var_Name='Background-Processing')")
 
   echo "MESSAGE $datetime: Starting videofromimage" >> /home/pi/log/$log.log
+  sudo rm -f /var/www/html/pss/files/temp.png
   pngimages=$(ls /var/www/html/pss/files/*.png 2>/dev/null)
   jpgimages=$(ls /var/www/html/pss/files/*.jpg 2>/dev/null)
   images="$pngimages $jpgimages"
@@ -24,8 +25,11 @@ then
     lorp=${fileid:-1}
     echo "MESSAGE $datetime: Converting $image to video" >> /home/pi/log/$log.log
 
+    sudo rm -f /var/www/html/pss/files/temp.png
+    sudo convert $image -resize 1920x1080 -background black -gravity center -extent 1920x1080 /var/www/html/pss/files/temp.png
+
     sudo rm -f /var/www/html/pss/files/temp.mp4
-    sudo ffmpeg -loop 1 -i $image -t 1 -pix_fmt yuv420p -vf scale=1920:1080 /var/www/html/pss/files/temp.mp4
+    sudo ffmpeg -loop 1 -i /var/www/html/pss/files/temp.png -t 1 -pix_fmt yuv420p -vf scale=1920:1080 /var/www/html/pss/files/temp.mp4
 
     if [[ -f "/var/www/html/pss/files/temp.mp4" ]]
     then
@@ -34,6 +38,7 @@ then
       sudo rm -f /var/www/html/pss/files/$file
     fi
     sudo rm -f /var/www/html/pss/files/temp.mp4
+    sudo rm -f /var/www/html/pss/files/temp.png
     sleep 2
   done
   
